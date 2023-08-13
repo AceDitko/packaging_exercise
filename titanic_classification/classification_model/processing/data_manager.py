9c9ebc1071d7dca3d510a2537c0df9d96fb10104
Jake Oliver <jacob.mj.oliver@gmail.com>
2023-08-13 16:53:23 +0100

    fix(data_manager.py): Fix misc typo

--- titanic_classification/classification_model/processing/data_manager.py
+++ titanic_classification/classification_model/processing/data_manager.py
@@ -105,7 +105,7 @@ def load_pipeline(*, file_name: str) -> Pipeline:
     return trained_model
 
 
-def remove_old_pipelines(*, files_to_keep: t.List[str]) -> None:
+def remove_old_pipelines(*, files_to_keep: List[str]) -> None:
     """
     Remove old model pipelines.
     This is to ensure there is a simple one-to-one
